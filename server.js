3b27d466ef65321cd70cbc40fe5a65caad2826ce
AbuPsng <135818554+AbuPsng@users.noreply.github.com>
2023-12-22 06:52:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(express.static(path.join(__dirname, "../client/build")))
+app.use(express.static(path.join(__dirname, "../client/dist")))
 
 //routes
 app.use("/api/v1/auth", authRouter)
@@ -35,7 +35,7 @@ app.use("/api/v1/product", productRouter)
 //
 // rest api
 app.get("*", function (req, res) {
-    res.sendFile(path.join("../client/dist/index.html"))
+    res.sendFile(path.join(__dirname"../client/dist/index.html"))
 })
 
 app.listen(parseInt(process.env.PORT), () => {
