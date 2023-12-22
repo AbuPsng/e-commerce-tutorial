a5c63e8eb4f7dc3373a73447e783e949fffafc8f
AbuPsng <135818554+AbuPsng@users.noreply.github.com>
2023-12-22 06:33:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRouter from "./routes/categoryRoute.js"
 import productRouter from "./routes/productRoute.js"
 import cors from "cors"
 import path from "path"
-import "fileURLToPath" from "URL"
+import {fileURLToPath} from "URL"
 
 //configure env
 dotenv.config()
